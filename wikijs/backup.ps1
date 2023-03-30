b206d0e8553f04b29df352a02474f7c7f389f972
Jordan Roher <jordan@notclickable.com>
2023-03-30 08:28:14 -0700

    Fixed wikijs backup

--- wikijs/backup.ps1
+++ wikijs/backup.ps1
@@ -10,7 +10,7 @@ $File1 = "backup-wikijs-db-data.tgz"
 # Backup existing volumes by tarring and gzipping them
 docker run --rm --volumes-from wikijs_db_data `
     -v ${pwd}:/backup ${Image} sh -c `
-    "tar cvzf /backup/${File1} /data"
+    "tar cvzf /backup/${File1} /var/lib/postgresql/data"
 
 # Copy to external drive and overwrite if files already exist
 Create-DatedDirectory -folderPath $BackupFolder
