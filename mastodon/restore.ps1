acb4c1eb2e3b202098dbde5ade77c6a357814bc8
Jordan Roher <jordan@notclickable.com>
2023-04-05 09:47:24 -0700

    Fixed mastodon restore

--- mastodon/restore.ps1
+++ mastodon/restore.ps1
@@ -5,19 +5,15 @@ $Folder1 = "/restore/${Volume1}}"
 
 $File2 = "mastodon-redis.tgz"
 $Volume2 = "mastodon_redis"
-$Folder2 = "/restore/${Volume1}}"
+$Folder2 = "/restore/${Volume2}"
 
 $File3 = "mastodon-dbs.tgz"
 $Volume3 = "mastodon_db"
-$Folder3 = "/restore/${Volume1}}"
+$Folder3 = "/restore/${Volume3}}"
 
 $File4 = "mastodon-letsencrypt.tgz"
 $Volume4 = "mastodon_letsencrypt"
-$Folder4 = "/restore/${Volume1}}"
-
-$File5 = "mastodon-letsencrypt_lib.tgz"
-$Volume5 = "mastodon_letsencrypt_lib"
-$Folder5 = "/restore/${Volume1}}"
+$Folder4 = "/restore/${Volume4}}"
 
 # Bring down the existing site
 docker compose down -v
@@ -32,7 +28,6 @@ docker run --rm `
     -v ${pwd}/${File2}:/backup/${File2} `
     -v ${pwd}/${File3}:/backup/${File3} `
     -v ${pwd}/${File4}:/backup/${File4} `
-    -v ${pwd}/${File5}:/backup/${File5} `
     -v ${Volume1}:${Folder1} `
     -v ${Volume2}:${Folder2} `
     -v ${Volume3}:${Folder3} `
@@ -42,8 +37,7 @@ docker run --rm `
         "tar -xvzf /backup/${File1} -C ${Folder1} && `
         tar -xvzf /backup/${File2} -C ${Folder2} && `
         tar -xvzf /backup/${File3} -C ${Folder3} && `
-        tar -xvzf /backup/${File4} -C ${Folder4} && `
-        tar -xvzf /backup/${File5} -C ${Folder5}"
+        tar -xvzf /backup/${File4} -C ${Folder4}"
 
 # Restore the site with the data
 docker compose up -d
