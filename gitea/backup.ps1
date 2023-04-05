98dc3504ecd103973bf8755aea29fa678904248a
Jordan Roher <jordan@notclickable.com>
2023-04-04 22:08:03 -0700

    Fun typos

--- gitea/backup.ps1
+++ gitea/backup.ps1
@@ -5,8 +5,8 @@ $File2 = "gitea-db.tgz"
 # Backup existing volumes by tarring and gzipping them
 docker run --rm --volumes-from mastodon_backup `
     -v ${pwd}:/backup ${Image} sh -c `
-    "tar -C /data -cvzf /backup/ .${File1} . && `
-    tar -C /var/lib/postgresql/data -cvzf /backup/ .${File2} ."
+    "tar -C /data -cvzf /backup/${File1} . && `
+    tar -C /var/lib/postgresql/data -cvzf /backup/${File2} ."
 
 # Copy to external drive and overwrite if files already exist
 Move-Item ".\${File1}" "${BackupFolder}\${dateString}\${File1}" -Force
