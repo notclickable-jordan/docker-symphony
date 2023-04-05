da646828d112347eea44e0140c462702ef93d02c
Jordan Roher <jordan@notclickable.com>
2023-04-04 22:06:31 -0700

    Trying another format

--- minecraft/backup.ps1
+++ minecraft/backup.ps1
@@ -4,7 +4,7 @@ $File1 = "minecraft-data.tgz"
 # Backup existing volumes by tarring and gzipping them
 docker run --rm --volumes-from minecraft_backup `
     -v ${pwd}:/backup ${Image} sh -c `
-    "tar -C /data cvzf /backup/${File1} ."
+    "tar -C /data -cvzf /backup/ .${File1} ."
 
 # Copy to external drive and overwrite if files already exist
 Move-Item ".\${File1}" "${BackupFolder}\${dateString}\${File1}" -Force
