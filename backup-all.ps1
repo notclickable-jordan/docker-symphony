59277c66e2eaf5dde68bc8fd0bea61069c651bab
Jordan Roher <jordan@notclickable.com>
2023-04-05 09:02:29 -0700

    Trying another script method

--- backup-all.ps1
+++ backup-all.ps1
@@ -7,13 +7,17 @@ $Image = "alpine:3.17.2"
 $BackupFolder = "D:\Backup\Not Clickable\docker\"
 Create-DatedDirectory -folderPath $BackupFolder
 
-# Get all PowerShell scripts in subfolders
-$scripts = Get-ChildItem -Path "." -Recurse -Include "backup.ps1"
-
-# Loop through each script and execute it
-foreach ($script in $scripts) {
-    & $_.FullName
-}
+# Run all backup scripts
+$PSScriptRoot/archivebox/backup.ps1
+$PSScriptRoot/calibre/backup.ps1
+$PSScriptRoot/gitea/backup.ps1
+$PSScriptRoot/mastodon/backup.ps1
+$PSScriptRoot/mealie/backup.ps1
+$PSScriptRoot/minecraft/backup.ps1
+$PSScriptRoot/miniflux/backup.ps1
+$PSScriptRoot/vaultwarden/backup.ps1
+$PSScriptRoot/vscode/backup.ps1
+$PSScriptRoot/wikijs/backup.ps1
 
 # Delete backups older than 7 days
 Get-ChildItem -Path $BackupFolder -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-7) } | Remove-Item -Force -Recurse
