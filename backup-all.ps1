5362b73b804db2cb87d294d8b0eb27351897afb9
Jordan Roher <jordan@notclickable.com>
2023-04-05 09:04:29 -0700

    Quotes?

--- backup-all.ps1
+++ backup-all.ps1
@@ -8,16 +8,16 @@ $BackupFolder = "D:\Backup\Not Clickable\docker\"
 Create-DatedDirectory -folderPath $BackupFolder
 
 # Run all backup scripts
-$PSScriptRoot/archivebox/backup.ps1
-$PSScriptRoot/calibre/backup.ps1
-$PSScriptRoot/gitea/backup.ps1
-$PSScriptRoot/mastodon/backup.ps1
-$PSScriptRoot/mealie/backup.ps1
-$PSScriptRoot/minecraft/backup.ps1
-$PSScriptRoot/miniflux/backup.ps1
-$PSScriptRoot/vaultwarden/backup.ps1
-$PSScriptRoot/vscode/backup.ps1
-$PSScriptRoot/wikijs/backup.ps1
+& "$PSScriptRoot/archivebox/backup.ps1"
+& "$PSScriptRoot/calibre/backup.ps1"
+& "$PSScriptRoot/gitea/backup.ps1"
+& "$PSScriptRoot/mastodon/backup.ps1"
+& "$PSScriptRoot/mealie/backup.ps1"
+& "$PSScriptRoot/minecraft/backup.ps1"
+& "$PSScriptRoot/miniflux/backup.ps1"
+& "$PSScriptRoot/vaultwarden/backup.ps1"
+& "$PSScriptRoot/vscode/backup.ps1"
+& "$PSScriptRoot/wikijs/backup.ps1"
 
 # Delete backups older than 7 days
 Get-ChildItem -Path $BackupFolder -Recurse | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-7) } | Remove-Item -Force -Recurse
