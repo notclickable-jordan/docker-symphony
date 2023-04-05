aac1a70d65d4987ba896aebbedc02534817b8f21
Jordan Roher <jordan@notclickable.com>
2023-04-05 10:04:22 -0700

    Vaultwarden cert fix?

--- vaultwarden/restore.ps1
+++ vaultwarden/restore.ps1
@@ -7,10 +7,6 @@ $File2 = "vaultwarden-letsencrypt.tgz"
 $Volume2 = "vaultwarden_letsencrypt"
 $Folder2 = "/restore/${Volume2}}"
 
-$File3 = "vaultwarden-letsencrypt_lib.tgz"
-$Volume3 = "vaultwarden_letsencrypt_lib"
-$Folder3 = "/restore/${Volume3}}"
-
 # Bring down the existing site
 docker compose down -v
 
@@ -21,15 +17,12 @@ docker compose down
 # Restore the volume data from the backups
 docker run --rm `
     -v ${pwd}/${File1}:/backup/${File1} `
-    -v ${pwd}/${File2}:/backup/${File3} `
-    -v ${pwd}/${File3}:/backup/${File2} `
+    -v ${pwd}/${File2}:/backup/${File2} `
     -v ${Volume1}:${Folder1} `
     -v ${Volume2}:${Folder2} `
-    -v ${Volume3}:${Folder3} `
     alpine:3.17.2 sh -c `
         "tar -xvzf /backup/${File1} -C ${Folder1} && `
-        tar -xvzf /backup/${File2} -C ${Folder2} && `
-        tar -xvzf /backup/${File3} -C ${Folder3}"
+        tar -xvzf /backup/${File2} -C ${Folder2}"
 
 # Restore the site with the data
 docker compose up -d
