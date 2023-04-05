2d39734bb4f768ecc1d8a2dd62e2858ce8e8c5e5
Jordan Roher <jordan@notclickable.com>
2023-04-04 22:22:42 -0700

    More fixes

--- gitea/backup.ps1
+++ gitea/backup.ps1
@@ -3,7 +3,7 @@ $File1 = "gitea-data.tgz"
 $File2 = "gitea-db.tgz"
 
 # Backup existing volumes by tarring and gzipping them
-docker run --rm --volumes-from mastodon_backup `
+docker run --rm --volumes-from gitea_backup `
     -v ${pwd}:/backup ${Image} sh -c `
     "tar -C /data -cvzf /backup/${File1} . && `
     tar -C /var/lib/postgresql/data -cvzf /backup/${File2} ."
