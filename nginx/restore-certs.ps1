250f6071a5136887d5c17627c5d26035c8f6a4d1
Jordan Roher <jordan@notclickable.com>
2023-04-13 13:03:44 -0700

    Fixed docker images Fixed restore function

--- nginx/restore-certs.ps1
+++ nginx/restore-certs.ps1
@@ -2,7 +2,7 @@
 $File1 = "homepage-letsencrypt.tgz"
 $File2 = "mastodon-letsencrypt.tgz"
 $File3 = "vaultwarden-letsencrypt.tgz"
-$VolumeName = "letsencrypt"
+$VolumeName = "nginx_letsencrypt"
 $FolderName = "/restore/${VolumeName}"
 
 # Bring down the existing site
@@ -18,10 +18,10 @@ docker run --rm `
     -v ${pwd}/${File2}:/backup/${File2} `
     -v ${pwd}/${File3}:/backup/${File3} `
     -v ${VolumeName}:${FolderName} `
-    alpine:3.17.2 sh -c `
-        "tar -xvzf /backup/${File1} -C ${FolderName} && `
-        tar -xvzf /backup/${File2} -C ${FolderName} && `
-        tar -xvzf /backup/${File3} -C ${FolderName}"
+    ubuntu:latest sh -c `
+        "tar -xvzf /backup/${File1} -C ${FolderName} --skip-old-files && `
+        tar -xvzf /backup/${File2} -C ${FolderName} --skip-old-files && `
+        tar -xvzf /backup/${File3} -C ${FolderName} --skip-old-files"
 
 # Restore the site with the data
 docker compose up -d
