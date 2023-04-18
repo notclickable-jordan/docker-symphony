3b6fa27d81a85d5b71a27717fa6ad08ebe9379a9
Jordan Roher <jordan@notclickable.com>
2023-04-18 15:57:39 -0700

    Fixed renewal script

--- nginx/renew.sh
+++ nginx/renew.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Untested
-
 # Run letsencrypt container and remove it after it exits
-docker-compose run --rm letsencrypt
+docker compose run --rm letsencrypt
 
 # Reload nginx configuration in the switchboard container
 docker exec switchboard nginx -s reload
