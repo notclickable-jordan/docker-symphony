161cee0d65ef5905e50a03870a097bc2c3624057
Jordan Roher <jordan@notclickable.com>
2023-04-18 08:48:13 -0700

    Calibre restore

---
+++ calibre/restore.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Variables
+File1="calibre-config.tgz"
+Volume1="calibre_config"
+Folder1="/restore/${Volume1}"
+
+# Bring down the existing site
+docker-compose down -v
+
+# Bring up the containers to recreate the volumes
+docker-compose up -d --build backup
+docker-compose down
+
+# Restore the volume data from the backups
+docker run --rm \
+    -v "$(pwd)/${File1}:/backup/${File1}" \
+    -v "${Volume1}:${Folder1}" \
+    alpine:3.17.2 sh -c \
+        "tar -xvzf /backup/${File1} -C ${Folder1}"
+
+# Restore the site with the data
+docker-compose up -d
