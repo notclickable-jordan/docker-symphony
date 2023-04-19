9248fad1aff40f711b1659f25f6f866a2a417364
Jordan Roher <jordan@notclickable.com>
2023-04-18 21:12:01 -0700

    Modifying get-certificate

--- nginx/get-certificate.sh
+++ nginx/get-certificate.sh
@@ -2,10 +2,11 @@
 
 subdomain="wallabag"
 
-docker run --rm -it \
-    --entrypoint="" \
+docker run --rm \
     -v letsencrypt:/etc/letsencrypt \
     -v certbot:/var/www/certbot \
-    -e TERM=xterm \
     certbot/certbot:v2.4.0 \
-    sh -c "certbot certonly --reinstall -n -v -d ${subdomain}.starbase80.dev --cert-name ${subdomain}.starbase80.dev --webroot --webroot-path /var/www/certbot/ --agree-tos --email jordan@notclickable.com"
+    certbot certonly --reinstall -n -v \
+    -d ${subdomain}.starbase80.dev \
+    --cert-name ${subdomain}.starbase80.dev \
+    --webroot --webroot-path /var/www/certbot/ --agree-tos --email jordan@notclickable.com
