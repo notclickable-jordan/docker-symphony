604f86267aa92abc65bd51896771474b085aef02
Jordan Roher <jordan@notclickable.com>
2023-06-29 14:44:15 -0700

    Setting path manually

--- backup-all.sh
+++ backup-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 
 # Log file path
 LOG_FILE="/home/jordan/cron/backup-all.log"
