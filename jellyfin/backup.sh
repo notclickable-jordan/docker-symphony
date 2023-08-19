f799fa701d7c7476fac15be2fcd70849c0b48299
Jordan Roher <jordan@notclickable.com>
2023-08-19 10:31:17 -0700

    Added Jellyfin backup

---
+++ jellyfin/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set variables
+BackupContainer="jellyfin"
+
+File1="jellyfin-config.tgz"
+Folder1="/config"
+
+File2="jellyfin-cache.tgz"
+Folder2="/cache"
+
+# Backup existing volumes by tarring and gzipping them
+docker run --rm --volumes-from ${BackupContainer} \
+    -v $(pwd):/backup ${Image} sh -c \
+    "tar -C ${Folder1} -cvzf /backup/${File1} . && \
+    tar -C ${Folder2} -cvzf /backup/${File2} ."
+
+# Copy to external drive and overwrite if files already exist
+cp "./${File1}" "${BackupFolder}/${dateString}/${File1}" -f
+cp "./${File2}" "${BackupFolder}/${dateString}/${File2}" -f
