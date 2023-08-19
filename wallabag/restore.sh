87e0d2b42b1e63d24e6b42d137dbb157c40a8478
Jordan Roher <jordan@notclickable.com>
2023-08-18 22:38:43 -0700

    Removed third non-existent Wallabag restore destination

--- wallabag/restore.sh
+++ wallabag/restore.sh
@@ -20,15 +20,11 @@ docker compose down
 docker run --rm \
     -v "$(pwd)/${File1}:/backup/${File1}" \
     -v "$(pwd)/${File2}:/backup/${File2}" \
-    -v "$(pwd)/${File3}:/backup/${File3}" \
     -v "${Volume1}:${Folder1}" \
     -v "${Volume2}:${Folder2}" \
-    -v "${Volume3}:${Folder3}" \
     alpine:3.17.2 sh -c \
         "tar -xvzf /backup/${File1} -C ${Folder1} && \
-        tar -xvzf /backup/${File2} -C ${Folder2} && \
-        tar -xvzf /backup/${File3} -C ${Folder3}"
-
+        tar -xvzf /backup/${File2} -C ${Folder2}"
 
 # Restore the site with the data
 docker compose up -d
