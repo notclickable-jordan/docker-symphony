d4a152a30933912cbf8531287f9c78c0c05b1786
Jordan Roher <jordan@notclickable.com>
2023-10-18 08:11:42 -0700

    Updated GitLab backup to include runner

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -12,14 +12,19 @@ Folder2="/var/log/gitlab"
 File3="gitlab-data.tgz"
 Folder3="/var/opt/gitlab"
 
+File4="gitlab-runner.tgz"
+Folder4="/etc/gitlab-runner"
+
 # Backup existing volumes by tarring and gzipping them
 docker run --rm --volumes-from ${BackupContainer} \
     -v $(pwd):/backup ${Image} sh -c \
     "tar -C ${Folder1} -cvzf /backup/${File1} . && \
     tar -C ${Folder2} -cvzf /backup/${File2} . && \
-    tar -C ${Folder3} -cvzf /backup/${File3} . "
+    tar -C ${Folder3} -cvzf /backup/${File3} . && \
+    tar -C ${Folder4} -cvzf /backup/${File4} . "
 
 # Copy to external drive and overwrite if files already exist
 cp "./${File1}" "${BackupFolder}/${dateString}/${File1}" -f
 cp "./${File2}" "${BackupFolder}/${dateString}/${File2}" -f
 cp "./${File3}" "${BackupFolder}/${dateString}/${File3}" -f
+cp "./${File4}" "${BackupFolder}/${dateString}/${File4}" -f
--- gitlab/restore.sh
+++ gitlab/restore.sh
@@ -13,6 +13,10 @@ File3="gitlab-data.tgz"
 Volume3="gitlab_data"
 Folder3="/restore/${Volume3}"
 
+File4="gitlab-runner.tgz"
+Volume4="gitlab_runner"
+Folder4="/restore/${Volume4}"
+
 # Bring down the existing site
 docker-compose down -v
 
@@ -25,13 +29,16 @@ docker run --rm \
     -v "$(pwd)/${File1}:/backup/${File1}" \
     -v "$(pwd)/${File2}:/backup/${File2}" \
     -v "$(pwd)/${File3}:/backup/${File3}" \
+    -v "$(pwd)/${File4}:/backup/${File4}" \
     -v "${Volume1}:${Folder1}" \
     -v "${Volume2}:${Folder2}" \
     -v "${Volume3}:${Folder3}" \
+    -v "${Volume4}:${Folder4}" \
     alpine:3.17.2 sh -c \
         "tar -xvzf /backup/${File1} -C ${Folder1} && \
         tar -xvzf /backup/${File2} -C ${Folder2} && \
-        tar -xvzf /backup/${File3} -C ${Folder3}"
+        tar -xvzf /backup/${File3} -C ${Folder3} && \
+        tar -xvzf /backup/${File4} -C ${Folder4}"
 
 # Restore the site with the data
 docker-compose up -d
